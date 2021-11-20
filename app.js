a1b32470a9cc935d2109397da45227363ff1ded5
Irina <airinlavis@gmail.com>
2021-11-20 14:05:11 +0200

    Get board

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const request = require('request');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  // res.send('Hello World!')
+  const options = {
+    method: 'GET',
+    url: 'https://api.miro.com/v2/boards/o9J_lhrJgtU%3D',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer T89n48o2pwdDCeMAovBY7_EJmBU'
+    }
+  };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
+  });
 })
+
 
 app.listen(process.env.PORT || 3787, 
 	() => console.log("Server is running..."));
